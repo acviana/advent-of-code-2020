6c251ecec650b46380e3fd5fe121f75625249ca2
Alex C. Viana <alexcostaviana@gmail.com>
2020-12-01 22:21:31 -0600

    Solition for day 1

---
+++ advent_of_code_2020/day_1.py
@@ -0,0 +1,27 @@
+from itertools import combinations
+from math import prod
+
+
+def load_data():
+    with open("advent_of_code_2020/input_1.txt", "r") as f:
+        return [int(item.strip()) for item in f.readlines()]
+
+
+def match_by_sum_and_multiply(input_list, set_size, match_sum):
+    for combination in combinations(input_list, set_size):
+        if sum(combination) == match_sum:
+            return combination
+
+
+def main():
+    data = load_data()
+
+    combination = match_by_sum_and_multiply(data, 2, 2020)
+    print(f"Set: {*combination,}, Sum: {sum(combination)}, Product: {prod(combination)}")
+
+    combination = match_by_sum_and_multiply(data, 3, 2020)
+    print(f"Set: {*combination,}, Sum: {sum(combination)}, Product: {prod(combination)}")
+
+
+if __name__ == '__main__':
+    main()
