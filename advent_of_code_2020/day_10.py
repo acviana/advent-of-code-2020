b6aee6861461f5ad444f7026560a335f58d95600
Alex C. Viana <alexcostaviana@gmail.com>
2020-12-12 10:28:05 -0600

    Day 10 Part 2

--- advent_of_code_2020/day_10.py
+++ advent_of_code_2020/day_10.py
@@ -1,5 +1,6 @@
-from collections import Counter
-from typing import List
+from collections import Counter, defaultdict
+from math import prod
+from typing import DefaultDict, Dict, List
 
 
 def load_data() -> List[str]:
@@ -7,25 +8,55 @@ def load_data() -> List[str]:
         return [item.strip() for item in f.readlines()]
 
 
-def parse_data(data: List[str]):
-    data = [int(item) for item in data]
-    data.sort()
-    return [0] + data + [data[-1] + 3]
+def parse_data(data: List[str]) -> List[int]:
+    output = [int(item) for item in data]
+    output.sort()
+    return [0] + output + [output[-1] + 3]
 
 
-def calc_differences(parsed_data):
+def calc_differences(parsed_data: List[int]) -> List[int]:
     return [
         parsed_data[counter + 1] - parsed_data[counter]
         for counter in range(0, len(parsed_data) - 1)
     ]
 
 
+def get_node_mappings(parsed_data: List[int]) -> Dict[int, List[int]]:
+    node_mappings = {
+        number: [item for item in range(number + 1, number + 4) if item in parsed_data]
+        for number in parsed_data
+    }
+    node_mappings[parsed_data[-1]] = [parsed_data[-1]]
+    return node_mappings
+
+
+def calc_step(current_data: DefaultDict[int, int], node_mappings: Dict[int, List[int]]):
+    new_data: DefaultDict[int, int] = defaultdict(int)
+    for node in current_data:
+        for next_node in node_mappings[node]:
+            new_data[next_node] += current_data[node]
+    return new_data
+
+
+def calc_all_paths(parsed_data: List[int]) -> int:
+    node_mappings = get_node_mappings(parsed_data)
+    current_data = defaultdict(int)
+    current_data[0] = 1
+    while not list(current_data.keys()) == [parsed_data[-1]]:
+        current_data = calc_step(current_data, node_mappings)
+    return current_data[parsed_data[-1]]
+
+
 def main() -> None:
     data = load_data()
     parsed_data = parse_data(data)
     differences = calc_differences(parsed_data)
+    print(prod(differences))
     count = Counter(differences)
     print(count[1] * count[3])
+
+    possible_paths = calc_all_paths(parsed_data)
+    print(f"Total possible paths: {possible_paths}")
 
 
 if __name__ == "__main__":
--- advent_of_code_2020/day_10_test.py
+++ advent_of_code_2020/day_10_test.py
@@ -1,4 +1,8 @@
-from advent_of_code_2020.day_10 import parse_data
+from advent_of_code_2020.day_10 import (
+    calc_differences,
+    calc_all_paths,
+    parse_data,
+)
 
 
 TEST_DATA = """16
@@ -13,7 +17,50 @@ TEST_DATA = """16
 12
 4"""
 TEST_DATA = [item for item in TEST_DATA.split("\n")]
+TEST_PARSED_DATA = [0, 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, 22]
+
+TEST_DATA_2 = """28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3"""
+TEST_DATA_2 = [item for item in TEST_DATA_2.split("\n")]
 
 
 def test_parse_data():
-    print(parse_data(TEST_DATA))
+    assert parse_data(TEST_DATA) == TEST_PARSED_DATA
+
+
+def test_calc_differences():
+    assert calc_differences(TEST_PARSED_DATA) == [1, 3, 1, 1, 1, 3, 1, 1, 3, 1, 3, 3]
+
+
+def test_calc_options():
+    calc_all_paths(TEST_PARSED_DATA) == 8
+    calc_all_paths(parse_data(TEST_DATA_2)) == 19208
