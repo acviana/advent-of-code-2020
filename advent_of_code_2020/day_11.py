74e74803f48a1b0ad9510f3fc1b2debd88df80cb
Alex C. Viana <alexcostaviana@gmail.com>
2020-12-15 23:20:20 -0600

    Added day 11 part 2

--- advent_of_code_2020/day_11.py
+++ advent_of_code_2020/day_11.py
@@ -8,7 +8,7 @@ def load_data() -> List[str]:
         return [item.strip() for item in f.readlines()]
 
 
-def get_data_slice(
+def get_nearest_neighbors_slice(
     row_position: int, column_position: int, data: List[str]
 ) -> List[str]:
     row_range_min = max(row_position - 1, 0)
@@ -21,31 +21,91 @@ def get_data_slice(
     ]
 
 
+def get_line_of_sight(
+    row_start_position: int,
+    column_start_position: int,
+    delta_row: int,
+    delta_column: int,
+    data: List[str],
+) -> str:
+    step = 1
+    next_position = ""
+    # Forgive me, it's late.
+    while 1 == 1:
+        row_position = row_start_position + (step * delta_row)
+        if row_position > len(data) - 1 or row_position < 0:
+            return next_position
+        column_position = column_start_position + (step * delta_column)
+        if column_position > len(data[0]) - 1 or column_position < 0:
+            return next_position
+        next_position = data[row_position][column_position]
+        if next_position in ["#", "L"]:
+            return next_position
+        else:
+            step += 1
+    assert False
+
+
+def get_line_of_sight_neighbors_slice(
+    row_position: int, column_position: int, data: List[str]
+) -> List[str]:
+    output = []
+    for delta_row in range(-1, 2):
+        for delta_column in range(-1, 2):
+            if not delta_row == 0 or not delta_column == 0:
+                line_of_sight = get_line_of_sight(
+                    row_position, column_position, delta_row, delta_column, data
+                )
+                if line_of_sight != "":
+                    output += [line_of_sight]
+    output += [data[row_position][column_position]]
+    return output
+
+
+def get_data_slice(
+    row_position: int, column_position: int, data: List[str], mode: str
+) -> List[str]:
+    if mode == "nearest_neighbors":
+        return get_nearest_neighbors_slice(row_position, column_position, data)
+    elif mode == "line_of_sight":
+        return get_line_of_sight_neighbors_slice(row_position, column_position, data)
+    else:
+        assert False
+
+
 def get_neighbors(data_slice: List[str], central_value: str):
     neighbors = Counter("".join(data_slice))
     neighbors[central_value] -= 1
     return neighbors
 
 
-def get_next_state(row_position: int, column_position: int, data: List[str]) -> str:
+def get_next_state(
+    row_position: int, column_position: int, data: List[str], mode: str
+) -> str:
+    if mode == "nearest_neighbors":
+        tolerance = 4
+    elif mode == "line_of_sight":
+        tolerance = 5
+    else:
+        assert False
     current_value = data[row_position][column_position]
     if current_value == ".":
         return "."
-    data_slice = get_data_slice(row_position, column_position, data)
+    data_slice = get_data_slice(row_position, column_position, data, mode)
     neighbors = get_neighbors(data_slice, current_value)
     if current_value == "L" and neighbors["#"] == 0:
         return "#"
-    elif current_value == "#" and neighbors["#"] >= 4:
+    elif current_value == "#" and neighbors["#"] >= tolerance:
         return "L"
     else:
         return current_value
 
 
-def get_next_data_set(data: List[str]) -> List[str]:
+def get_next_data_set(data: List[str], mode: str) -> List[str]:
     return [
         "".join(
             [
-                get_next_state(row_position, column_position, data)
+                get_next_state(row_position, column_position, data, mode)
                 for column_position in range(0, len(data[0]))
             ]
         )
@@ -53,11 +113,11 @@ def get_next_data_set(data: List[str]) -> List[str]:
     ]
 
 
-def get_final_state(current_data: List[str]) -> List[str]:
-    next_data = get_next_data_set(current_data)
+def get_final_state(current_data: List[str], mode: str) -> List[str]:
+    next_data = get_next_data_set(current_data, mode)
     while not current_data == next_data:
         current_data = copy.deepcopy(next_data)
-        next_data = get_next_data_set(current_data)
+        next_data = get_next_data_set(current_data, mode)
     return current_data
 
 
@@ -67,7 +127,11 @@ def count_occupied_seats(data: List[str]) -> int:
 
 def main() -> None:
     data = load_data()
-    final_state = get_final_state(data)
+    final_state = get_final_state(data, mode="nearest_neighbors")
+    occupied_seats = count_occupied_seats(final_state)
+    print(f"Occupied Seats: {occupied_seats}")
+
+    final_state = get_final_state(data, mode="line_of_sight")
     occupied_seats = count_occupied_seats(final_state)
     print(f"Occupied Seats: {occupied_seats}")
 
--- advent_of_code_2020/day_11_test.py
+++ advent_of_code_2020/day_11_test.py
@@ -4,6 +4,7 @@ from advent_of_code_2020.day_11 import (
     count_occupied_seats,
     get_data_slice,
     get_final_state,
+    get_line_of_sight,
     get_neighbors,
     get_next_data_set,
     get_next_state,
@@ -57,50 +58,183 @@ L.#.L..#..
 #.#L#L#.##"""
 TEST_DATA_FINAL = [item.strip() for item in TEST_DATA_FINAL.split("\n")]
 
+TEST_DATA_EXAMPLE_1_LOS = """.......#.
+...#.....
+.#.......
+.........
+..#L....#
+....#....
+.........
+#........
+...#....."""
+TEST_DATA_EXAMPLE_1_LOS = [item.strip() for item in TEST_DATA_EXAMPLE_1_LOS.split("\n")]
+
+TEST_DATA_EXAMPLE_2_LOS = """.............
+.L.L.#.#.#.#.
+............."""
+TEST_DATA_EXAMPLE_2_LOS = [item.strip() for item in TEST_DATA_EXAMPLE_2_LOS.split("\n")]
+
+TEST_DATA_EXAMPLE_3_LOS = """.##.##.
+#.#.#.#
+##...##
+...L...
+##...##
+#.#.#.#
+.##.##."""
+TEST_DATA_EXAMPLE_3_LOS = [item.strip() for item in TEST_DATA_EXAMPLE_3_LOS.split("\n")]
+
+TEST_DATA_3_LOS = """#.LL.LL.L#
+#LLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLLL.L
+#.LLLLL.L#"""
+TEST_DATA_3_LOS = [item.strip() for item in TEST_DATA_3_LOS.split("\n")]
+
+TEST_DATA_4_LOS = """#.LL.LL.L#
+#LLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLLL.L
+#.LLLLL.L#"""
+TEST_DATA_4_LOS = [item.strip() for item in TEST_DATA_4_LOS.split("\n")]
+
+
+TEST_DATA_FINAL_LOS = """#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.LL.L#
+#.LLLL#.LL
+..#.L.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#"""
+TEST_DATA_FINAL_LOS = [item.strip() for item in TEST_DATA_FINAL_LOS.split("\n")]
+
+
+def test_get_line_of_sight():
+    assert get_line_of_sight(4, 3, 1, 0, TEST_DATA_EXAMPLE_1_LOS) == "#"
+    assert get_line_of_sight(4, 3, -1, 0, TEST_DATA_EXAMPLE_1_LOS) == "#"
+    assert get_line_of_sight(4, 3, 0, 1, TEST_DATA_EXAMPLE_1_LOS) == "#"
+    assert get_line_of_sight(4, 3, 0, -1, TEST_DATA_EXAMPLE_1_LOS) == "#"
+    assert get_line_of_sight(4, 3, 1, 1, TEST_DATA_EXAMPLE_1_LOS) == "#"
+    assert get_line_of_sight(4, 3, 1, -1, TEST_DATA_EXAMPLE_1_LOS) == "#"
+    assert get_line_of_sight(4, 3, -1, 1, TEST_DATA_EXAMPLE_1_LOS) == "#"
+    assert get_line_of_sight(4, 3, -1, -1, TEST_DATA_EXAMPLE_1_LOS) == "#"
+
+    assert get_line_of_sight(0, 0, 1, 0, TEST_DATA_2) == "#"
+    assert get_line_of_sight(0, 0, -1, 0, TEST_DATA_2) == ""
+    assert get_line_of_sight(0, 0, 0, 1, TEST_DATA_2) == "#"
+    assert get_line_of_sight(0, 0, 0, -1, TEST_DATA_2) == ""
+    assert get_line_of_sight(0, 0, 1, 1, TEST_DATA_2) == "#"
+    assert get_line_of_sight(0, 0, 1, -1, TEST_DATA_2) == ""
+    assert get_line_of_sight(0, 0, -1, 1, TEST_DATA_2) == ""
+    assert get_line_of_sight(0, 0, -1, -1, TEST_DATA_2) == ""
+
 
 def test_get_data_slice():
-    assert get_data_slice(5, 5, TEST_DATA_1) == [".LL", "LLL", "L.."]
-    assert get_data_slice(0, 0, TEST_DATA_1) == ["L.", "LL"]
-    assert get_data_slice(0, 9, TEST_DATA_1) == ["LL", "LL"]
-    assert get_data_slice(9, 0, TEST_DATA_1) == ["L.", "L."]
-    assert get_data_slice(9, 9, TEST_DATA_1) == [".L", "LL"]
+    assert get_data_slice(5, 5, TEST_DATA_1, "nearest_neighbors") == [
+        ".LL",
+        "LLL",
+        "L..",
+    ]
+    assert get_data_slice(0, 0, TEST_DATA_1, "nearest_neighbors") == ["L.", "LL"]
+    assert get_data_slice(0, 9, TEST_DATA_1, "nearest_neighbors") == ["LL", "LL"]
+    assert get_data_slice(9, 0, TEST_DATA_1, "nearest_neighbors") == ["L.", "L."]
+    assert get_data_slice(9, 9, TEST_DATA_1, "nearest_neighbors") == [".L", "LL"]
+
+    expected_result = ["#", "#", "#", "#", "#", "#", "#", "#", "L"]
+    assert (
+        get_data_slice(4, 3, TEST_DATA_EXAMPLE_1_LOS, "line_of_sight")
+        == expected_result
+    )
+
+    assert get_data_slice(0, 0, TEST_DATA_2, "line_of_sight") == ["#", "#", "#", "#"]
+    assert get_data_slice(2, 0, TEST_DATA_2, "line_of_sight") == [
+        "#",
+        "#",
+        "#",
+        "#",
+        "#",
+        "#",
+    ]
 
 
 def test_get_neighbors():
     assert get_neighbors(
-        get_data_slice(5, 5, TEST_DATA_1), TEST_DATA_1[5][5]
+        get_data_slice(5, 5, TEST_DATA_1, "nearest_neighbors"), TEST_DATA_1[5][5]
     ) == Counter({"L": 5, ".": 3})
     assert get_neighbors(
-        get_data_slice(0, 0, TEST_DATA_1), TEST_DATA_1[0][0]
+        get_data_slice(0, 0, TEST_DATA_1, "nearest_neighbors"), TEST_DATA_1[0][0]
     ) == Counter({"L": 2, ".": 1})
     assert get_neighbors(
-        get_data_slice(0, 9, TEST_DATA_1), TEST_DATA_1[0][9]
+        get_data_slice(0, 9, TEST_DATA_1, "nearest_neighbors"), TEST_DATA_1[0][9]
     ) == Counter({"L": 3})
     assert get_neighbors(
-        get_data_slice(9, 0, TEST_DATA_1), TEST_DATA_1[9][0]
+        get_data_slice(9, 0, TEST_DATA_1, "nearest_neighbors"), TEST_DATA_1[9][0]
     ) == Counter({".": 2, "L": 1})
     assert get_neighbors(
-        get_data_slice(9, 9, TEST_DATA_1), TEST_DATA_1[9][9]
+        get_data_slice(9, 9, TEST_DATA_1, "nearest_neighbors"), TEST_DATA_1[9][9]
     ) == Counter({"L": 2, ".": 1})
+
+    assert (
+        get_neighbors(
+            get_data_slice(4, 3, TEST_DATA_EXAMPLE_1_LOS, "line_of_sight"),
+            TEST_DATA_EXAMPLE_1_LOS[4][3],
+        )
+        == Counter({"#": 8, "L": 0})
+    )
+
+    assert (
+        get_neighbors(
+            get_data_slice(1, 1, TEST_DATA_EXAMPLE_2_LOS, "line_of_sight"),
+            TEST_DATA_EXAMPLE_2_LOS[1][1],
+        )
+        == Counter({".": 7, "L": 1})
+    )
+
+    assert (
+        get_neighbors(
+            get_data_slice(3, 3, TEST_DATA_EXAMPLE_3_LOS, "line_of_sight"),
+            TEST_DATA_EXAMPLE_3_LOS[3][3],
+        )
+        == Counter({".": 8, "L": 0})
+    )
 
 
 def test_get_next_state():
-    assert get_next_state(5, 5, TEST_DATA_1) == "#"
-    assert get_next_state(0, 0, TEST_DATA_1) == "#"
-    assert get_next_state(0, 9, TEST_DATA_1) == "#"
-    assert get_next_state(9, 0, TEST_DATA_1) == "#"
-    assert get_next_state(9, 9, TEST_DATA_1) == "#"
+    assert get_next_state(5, 5, TEST_DATA_1, "nearest_neighbors") == "#"
+    assert get_next_state(0, 0, TEST_DATA_1, "nearest_neighbors") == "#"
+    assert get_next_state(0, 9, TEST_DATA_1, "nearest_neighbors") == "#"
+    assert get_next_state(9, 0, TEST_DATA_1, "nearest_neighbors") == "#"
+    assert get_next_state(9, 9, TEST_DATA_1, "nearest_neighbors") == "#"
+
+    assert get_next_state(2, 0, TEST_DATA_2, "line_of_sight") == "L"
+    assert get_next_state(1, 0, TEST_DATA_2, "line_of_sight") == "#"
 
 
 def test_get_next_data_set():
-    assert get_next_data_set(TEST_DATA_1) == TEST_DATA_2
-    assert get_next_data_set(TEST_DATA_2) == TEST_DATA_3
+    assert get_next_data_set(TEST_DATA_1, "nearest_neighbors") == TEST_DATA_2
+    assert get_next_data_set(TEST_DATA_2, "nearest_neighbors") == TEST_DATA_3
+
+    assert get_next_data_set(TEST_DATA_1, "line_of_sight") == TEST_DATA_2
+    assert get_next_data_set(TEST_DATA_2, "line_of_sight") == TEST_DATA_3_LOS
 
 
 def test_get_final_state():
-    assert get_final_state(TEST_DATA_1) == TEST_DATA_FINAL
+    assert get_final_state(TEST_DATA_1, "nearest_neighbors") == TEST_DATA_FINAL
+    assert get_final_state(TEST_DATA_1, "line_of_sight") == TEST_DATA_FINAL_LOS
 
 
 def test_count_occupied_seats():
-    final_state = get_final_state(TEST_DATA_1)
+    final_state = get_final_state(TEST_DATA_1, "nearest_neighbors")
     assert count_occupied_seats(final_state) == 37
